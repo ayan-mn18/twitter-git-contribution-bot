53a3dde30b523c65585465b6870ac7fee69c1fad
ayan-mn18 <ayanmansuri2001@gmail.com>
2024-11-17 22:37:21 +0530

    My bot posting tweets :)

--- server.ts
+++ server.ts
@@ -1,7 +1,38 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen('3000', () => {
-  console.log('Hello Twitter BOT')
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+import { TwitterApi } from 'twitter-api-v2';
+
+
+// Load credentials from environment variables
+const client = new TwitterApi({
+  appKey: process.env.X_API_KEY!,
+  appSecret: process.env.X_API_SECRET!,
+  accessToken: process.env.X_ACCESS_TOKEN!,
+  accessSecret: process.env.X_ACCESS_SECRET!,
+});
+
+const postTweet = async (message: string) => {
+  try {
+    const response = await client.v2.tweet(message);
+    console.log('Tweet posted:', response);
+  } catch (error) {
+    console.error('Error posting tweet:', error);
+  }
+};
+
+app.get('/post', (req,res) => {
+  postTweet('This is my bot tweeting for me :)')
+  return;
 })
+  
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+  
