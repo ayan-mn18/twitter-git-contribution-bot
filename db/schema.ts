0bcad38ace34efda05d624438519ac1056653211
ayan-mn18 <ayanmansuri2001@gmail.com>
2024-11-21 00:14:12 +0530

    Added Schemas

--- db/schema.ts
+++ db/schema.ts
@@ -7,17 +7,51 @@ import {
 } from 'drizzle-orm/sqlite-core';
 import { sql } from 'drizzle-orm';
 
+// User schema
 export const users = sqliteTable('users', {
-  id: text('user_id').primaryKey(),
-  email: text('email').notNull().unique(),
-  // subscriptionId: text('subscription_id').references(() => subscriptions.id),
+  userId: text('user_id').primaryKey(),
+  email: text('email').notNull().unique(), 
+  xCredId: text('x_cred_id').references(() => xCred.id),
+  twitterUsername: text('twitter_username'),
+  github: text('github').references(() => platformCred.id),
+  leetcode: text('leetcode').references(() => platformCred.id),
+  bitbucket: text('bitbucket').references(() => platformCred.id),
+  tweetsGenerated: text('tweets_generated', { mode: 'json' })
+                  .$type<string[]>()
+                  .references(() => tweet.id), 
+  timezone: text({ enum: ["IST", "UTC", "GMT"] }), 
+  jobFrequency: text( 'job_frequency', { enum: ["IST", "UTC", "GMT"] }),
+  jobStartTime: text('job_start_time', { enum: ["24hrs", "12hrs", "6hrs"] }), 
   createdAt: integer('created_at', { mode: 'timestamp' }).default(
-      sql`CURRENT_TIMESTAMP`
+    sql`CURRENT_TIMESTAMP`
   ),
   updatedAt: integer('updated_at', { mode: 'timestamp' }).default(
-      sql`CURRENT_TIMESTAMP`
+    sql`CURRENT_TIMESTAMP`
   ),
 });
 
+// xCred schema
+export const xCred = sqliteTable('x_cred', {
+  id: text('id').primaryKey(),
+  apiKey: text('api_key').notNull(),
+  apiSecret: text('api_secret').notNull(),
+  accessToken: text('access_token').notNull(),
+  accessSecret: text('access_secret').notNull(),
+});
+
+export const platformCred = sqliteTable('platform_cred', {
+  id: text('id').primaryKey(),
+  active: integer({ mode: 'boolean' }).notNull(),
+  username: text('username').notNull(),
+});
+
+export const tweet = sqliteTable('tweets', {
+  id: text('id').primaryKey(),
+  content: text('content').notNull(),
+  postedAt: integer('posted_at', { mode: 'timestamp' }).notNull(),
+});
 
 export type User = typeof users.$inferSelect;
+export type XCred = typeof xCred.$inferSelect;
+export type PlatformCred = typeof platformCred.$inferSelect;
+export type Tweet = typeof tweet.$inferSelect;
